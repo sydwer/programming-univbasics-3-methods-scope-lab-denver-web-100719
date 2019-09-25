0e0cd634690bb807cbb33191c7883d5882f7b9b1
Sydney Werbach <sydwer@gmail.com>
2019-09-25 02:55:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,4 +9,12 @@ def toadstool
 end
 
 def link
-  he_a_child = 
+  he_a_child = "It's Dangerous To Go Alone! Take This."
+  puts he_a_child
+end
+
+def any_phrase(phrase)
+  your_input_is_unecessary = "Do A Barrel Roll!"
+  puts your_input_is_unecessary
+end
+  
